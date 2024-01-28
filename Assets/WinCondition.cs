afa3e3b026b335eb02530e31521a194b8a35f7f4
joonasman <joonas.manninen98@gmail.com>
2024-01-28 13:55:42 +0200

    win work now
    yippee

---
+++ Assets/WinCondition.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinCondition : MonoBehaviour
+{
+    private KillZone killzone;
+
+    private void Awake()
+    {
+        killzone = GameObject.FindGameObjectWithTag("kikeli").GetComponent<KillZone>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") == true)
+        {
+            killzone.GetComponent<KillZone>().IsWin = true;
+            Debug.Log("hihihhii kutittaa voitit pelin");
+            
+            SceneManager.LoadScene("GameWin");
+
+        }
+    }
+}
