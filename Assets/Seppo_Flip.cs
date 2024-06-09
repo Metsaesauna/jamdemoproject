c0fb3dbe33e668c27d211d57673008cb7d96d88b
JonesMoney <joonas.manninen98@gmail.com>
2024-06-09 13:35:49 +0300

    fixed seppoflip

--- Assets/Seppo_Flip.cs
+++ Assets/Seppo_Flip.cs
@@ -4,27 +4,23 @@ using UnityEngine;
 
 public class Seppo_Flip : MonoBehaviour
 {
-    public float flipInterval = 5000f;
+    private float flipInterval = 0.5f; // Interval in seconds
 
     private float nextFlipTime;
-    private SpriteRenderer SpriteRenderer;
+    private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
-        nextFlipTime = Time.time;
-
-        SpriteRenderer = GetComponent<SpriteRenderer>();
-        
+        nextFlipTime = Time.time + flipInterval;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        if (Time.time >= nextFlipTime) 
-        { 
-            SpriteRenderer.flipX = !SpriteRenderer.flipX;
-
-            nextFlipTime += Time.time + flipInterval;
+        if (Time.time >= nextFlipTime)
+        {
+            spriteRenderer.flipX = !spriteRenderer.flipX;
+            nextFlipTime += flipInterval; // Corrected line to add the interval to the next flip time
         }
     }
-
 }
